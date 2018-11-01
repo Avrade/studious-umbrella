0cfadc6cd8a1d0b2a088dcc2e4e627fb48e1d812
Julien <mansjulien@gmail.com>
2018-11-01 18:44:39 +0100

    First commit

---
+++ test.py
@@ -0,0 +1,18 @@
+import pygame
+
+
+#Se lance au debut du programme
+grid = [[0 for i in range(10)] for j in range(20)]
+
+print(grid)
+#Tourne dans le vide
+def loop():
+    pass
+
+def draw():
+    for i in grid:
+        print(i)
+
+#Main
+while True:
+    loop()
